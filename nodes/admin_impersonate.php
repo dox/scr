5afb5512510b71ac49da7eca91558e3f107de1ec
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-12-04 16:49:48 +0000

    ability to impersonate other users

--- nodes/admin_impersonate.php
+++ nodes/admin_impersonate.php
@@ -3,19 +3,45 @@
     <h1 class="display-4">Impersonate</h1>
     <p class="lead">Assume the identity of an SCR Member for the purposes of managing their meal bookings.</p>
   </div>
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-4">
+      <form method="post" id="impersonateForm" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+        <div class="mb-3">
+          <?php
+          if (isset($_SESSION['impersonating'])) {
+            $selectStatus = " disabled";
+          }
+          ?>
+          <select <?php echo $selectStatus; ?> class="form-select form-select mb-3" name="impersonate_ldap" aria-label="Impersonate select">
+            <?php
+            $membersClass = new members();
 
-Coming soon...
+            $members = $membersClass->all();
+            foreach ($members AS $member) {
+              $memberObject = new member($member['uid']);
 
-<select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-  <?php
-  $membersClass = new members();
+              if (isset($_SESSION['impersonating']) && $_SESSION['username'] == $memberObject->ldap) {
+                $selectStatus = " selected";
+              } else {
+                $selectStatus = "";
+              }
 
-  $members = $membersClass->all();
-  foreach ($members AS $member) {
-    $memberObject = new member($member['uid']);
-
-    echo "<option selected>" . $memberObject->displayName() . "</option>";
-  }
-  ?>
-</select>
+              echo "<option " . $selectStatus . " value=\"" . $memberObject->ldap . "\">" . $memberObject->displayName() . "</option>";
+            }
+            ?>
+          </select>
+        </div>
+        <div class="d-grid gap-2">
+          <?php
+          if (isset($_SESSION['impersonating'])) {
+            echo "<button type=\"submit\" class=\"btn btn-warning\">Stop Impersonating</button>";
+            echo "<input type=\"hidden\" name=\"stop_impersonating\" value=\"true\">";
+          } else {
+            echo "<button type=\"submit\" class=\"btn btn-primary\">Impersonate</button>";
+          }
+          ?>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
