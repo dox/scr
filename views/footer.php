4c9c3fdd7b243be891b60a6db7c246232fc5952e
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-12-31 16:07:12 +0000

    Missing the chough beak!

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 <footer class="pt-4 my-md-5 pt-md-5 border-top">
   <div class="row">
     <div class="col-8">
-      <svg width="18" height="16" class="text-muted">
+      <svg width="19" height="16" class="text-muted">
 				<use xlink:href="img/icons.svg#chough-regular"/>
 			</svg>
       <a class="link-secondary" href="https://github.com/dox/scr">SCR Meal Booking</a>
--- views/header.php
+++ views/header.php
@@ -2,30 +2,30 @@
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container">
 	  <a class="navbar-brand" href="index.php">
-			<svg width="28" height="24">
+			<svg width="29" height="24">
 				<use xlink:href="img/icons.svg#chough-regular"/>
 			</svg> SCR Meal Booking</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLinkCollapse" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLinkCollapse" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
 	  <div class="collapse navbar-collapse" id="navbarLinkCollapse">
 	    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-	      <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">
+	      <li class="nav-item d-print-none"><a class="nav-link active" aria-current="page" href="index.php">
 					<svg width="16" height="16" class="text-muted">
 						<use xlink:href="img/icons.svg#house-door"/>
 					</svg> Home</a>
 	      </li>
-	      <li class="nav-item"><a class="nav-link" aria-current="page" href="index.php?n=member">
+	      <li class="nav-item d-print-none"><a class="nav-link" aria-current="page" href="index.php?n=member">
 					<svg width="16" height="16" class="text-muted">
 						<use xlink:href="img/icons.svg#person"/>
 					</svg> Your Profile</a>
 	      </li>
-	      <li class="nav-item"><a class="nav-link" aria-current="page" href="index.php?n=member">
+	      <li class="nav-item d-print-none"><a class="nav-link" aria-current="page" href="index.php?n=member">
 					<svg width="16" height="16" class="text-muted">
 						<use xlink:href="img/icons.svg#list-stars"/>
 					</svg> Your Meals</a>
 	      </li>
-	      <li class="nav-item"><a class="nav-link" aria-current="page" href="index.php?n=information">
+	      <li class="nav-item d-print-none"><a class="nav-link" aria-current="page" href="index.php?n=information">
 					<svg width="16" height="16" class="text-muted">
 						<use xlink:href="img/icons.svg#journal-text"/>
 					</svg> SCR Information</a>
@@ -34,9 +34,13 @@
 	    <div class="d-flex">
 	      <ul class="navbar-nav mr-auto">
 	        <li class="nav-item dropdown">
-	          <a class="nav-link dropdown-toggle" href="#" id="navbarAvatarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	            <?php echo $_SESSION['username']; ?>
-	            <!--<div class="small text-muted"><?php echo $_SESSION['username']; ?></div>-->
+	          <a class="nav-link dropdown-toggle" href="#" id="navbarAvatarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	            <?php
+							if ($_SESSION['impersonating'] == true) {
+									echo "<button class=\"btn btn-sm btn-warning\">IMPERSONATING</button> ";
+							}
+							echo $_SESSION['username'];
+							?>
 	          </a>
 	          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 	            <?php if($_SESSION['admin'] == true) { ?><a class="dropdown-item" href="index.php?n=admin_impersonate">Impersonate</a><?php } ?>
