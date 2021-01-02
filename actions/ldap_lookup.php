82908f934179716dad270d4e414c9a6be0911ed6
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2021-01-02 17:41:00 +0000

    Only admins should be able to perform LDAP lookups
    Also - log!

--- actions/ldap_lookup.php
+++ actions/ldap_lookup.php
@@ -1,22 +1,30 @@
 <?php
 include_once("../inc/autoload.php");
 
-if (!empty($_POST['firstname'])) {
-  $ldapUser = $ldap_connection->query()
-  ->where('givenname', '~=', $_POST['firstname'])
-  ->where('sn', '~=', $_POST['sn'])
-  ->first();
+if ($_SESSION['admin'] == true) {
+  if (!empty($_POST['firstname'])) {
+    $ldapUser = $ldap_connection->query()
+    ->where('givenname', '~=', escape($_POST['firstname']))
+    ->where('sn', '~=', escape($_POST['sn']))
+    ->first();
+  } else {
+    $ldapUser = $ldap_connection->query()
+    ->where('sn', '~=', escape($_POST['sn']))
+    ->first();
+  }
+
+  $bestGuessLDAP = $ldapUser['samaccountname'][0];
+  $logsClass->create("ldap", "LDAP lookup permitted for " . escape($_POST['firstname']) . "/" . escape($_POST['sn']));
+
+  if (empty($bestGuessLDAP)) {
+    echo "Unknown - please try a different firstname/lastname.";
+  } else {
+    echo $bestGuessLDAP;
+  }
 } else {
-  $ldapUser = $ldap_connection->query()
-  ->where('sn', '~=', $_POST['sn'])
-  ->first();
+  $logsClass->create("ldap", "LDAP lookup for denied for " . escape($_POST['firstname']) . "/" . escape($_POST['sn']));
+  echo "Unknown - you do not have permission to perform LDAP lookups.";
 }
 
-$bestGuessLDAP = $ldapUser['samaccountname'][0];
 
-if (empty($bestGuessLDAP)) {
-  echo "Unknown - please try a different firstname/lastname.";
-} else {
-  echo $bestGuessLDAP;
-}
 ?>
