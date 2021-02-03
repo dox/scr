076ef5665e927802b6f1bd6e8701552130b3adbd
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2021-02-03 15:57:37 +0000

    Better footer layout on smaller screens

--- views/footer.php
+++ views/footer.php
@@ -1,14 +1,14 @@
 <div class="container">
 <footer class="pt-4 my-md-5 pt-md-5 border-top">
   <div class="row">
-    <div class="col-8">
+    <div class="col-6">
       <svg width="1.2em" height="1em" class="text-muted">
 				<use xlink:href="img/icons.svg#chough-regular"/>
 			</svg>
       <a class="link-secondary" href="https://github.com/dox/scr">SCR Meal Booking</a>
       <small class="d-block mb-3 text-muted">&copy; 2008-<?php echo date('Y'); ?></small>
     </div>
-    <div class="col-4 text-end">
+    <div class="col-6 text-end">
       <ul class="list-unstyled text-small">
         <li><a class="link-secondary" href="index.php?n=accessibility">Accessibility Statement</a></li>
       </ul>
