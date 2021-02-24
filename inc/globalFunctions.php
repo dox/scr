0c72ac8cf9aad31da288577410e000438f428315
Andrew Breakspear <andrew@longhanborough.com>
2021-02-24 11:38:58 +0000

    Removing redundant code from makteTitle function

--- inc/globalFunctions.php
+++ inc/globalFunctions.php
@@ -200,29 +200,6 @@ function admin_gatekeeper() {
 }
 
 function makeTitle($title = null, $subtitle = nulll, $iconsArray = null) {
-
-
-/*
-              <!-- Page title actions -->
-              <div class="col-auto ms-auto d-print-none">
-                <div class="btn-list">
-                  <span class="d-none d-sm-inline">
-                    <a href="#" class="btn btn-white">
-                      New view
-                    </a>
-                  </span>
-                  <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-                    Create new report
-                  </a>
-                  <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-                  </a>
-                </div>
-              </div>
-
-*/
-
 	$output  = "<div class=\"px-3 py-3 pt-md-5 pb-md-4 text-center\">";
 	$output .= "<h1 class=\"display-4\">" . $title . "</h1>";
 
