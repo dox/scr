23ae3668fd1c646f9d653199d661a10a37c38129
Andrew Breakspear <andrew@longhanborough.com>
2021-04-13 17:19:47 +0100

    404 now logs properly

--- nodes/404.php
+++ nodes/404.php
@@ -1,8 +1,10 @@
 <?php
-$logsClass->create("view_fail", $_SERVER['REQUEST_URI'] . " viewed but didn't exist");
+$logArray['category'] = "view";
+$logArray['result'] = "danger";
+$logArray['description'] = $_SERVER['REQUEST_URI'] . " viewed but didn't exist";
+$logsClass->create($logArray);
 ?>
-<div class="container">
+
 <h1 class="display-1 text-center"><strong>404</strong> Page not found</h1>
 <p class="text-center">The page you are looking for <code>'<?php echo $_SERVER['REQUEST_URI'];?>'</code> isn't available.</p>
 <p class="text-center">Please report this to <a href="mailto:<?php echo support_email;?>"><?php echo support_email;?></a>.
-</div>
