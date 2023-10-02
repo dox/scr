dbe2b5636f264278ffa6344b967ce80e7680cece
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2023-10-02 19:23:40 +0100

    Minor adjustments to new logon screen

--- logon2.php
+++ logon2.php
@@ -17,34 +17,37 @@ if (isset($_GET['logout'])) {
 					<div class="px-5 ms-xl-4">
 						<img src="/img/logo.png" alt="Site Logo" width="100px" class="me-3 pt-5 mt-xl-4" />
 						<h1 class="pt-4"><?php echo site_name; ?></h1>
-						<?php
-						if (debug) {
-							$output  = "<div class=\"alert alert-warning\" role=\"alert\">";
-							$output .= "<strong>Warning!</strong> This site is in <code>debug mode</code>.  It is for testing purposes only!";
-							$output .= "</div>";
-							echo $output;
-						}
-						?>
+						
 					</div>
 					
 					<div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
 						<form style="width: 23rem;" method="post" id="loginSubmit" action="index.php">
-							<div class="form-outline mb-4">
+							<div class=" mb-4">
+								<label class="form-label" for="inputUsername">Username</label>
 								<input type="text" id="inputUsername" name="inputUsername" class="form-control form-control-lg" />
-								<label class="form-label" for="form2Example18">Username</label>
 							</div>
 							
-							<div class="form-outline mb-4">
+							<div class="mb-4">
+								<label class="form-label" for="inputPassword">Password</label>
 								<input type="password" id="inputPassword" name="inputPassword" class="form-control form-control-lg" />
-								<label class="form-label" for="form2Example28">Password</label>
 							</div>
 							
-							<div class="pt-1 mb-4">
-								<button class="btn btn-info btn-lg btn-block" type="submit">Login</button>
+							<div class="pt-1 mb-4 d-grid gap-2">
+								<button class="btn btn-primary btn-lg" type="submit">Login</button>
+								<p class="mb-4 pb-lg-2"><a class="text-muted" href="<?php echo reset_url; ?>">Forgot password?</a></p>
 							</div>
 							
-							<p class="mb-5 pb-lg-2" ><a class="text-muted" href="<?php echo reset_url; ?>">Forgot password?</a></p>
-							<p class="text-muted small">Developed by <a href="https://github.com/dox">Andrew Breakspear</a></p>
+							<div class="pt-1 mb-4 d-grid gap-2">
+								<p class="text-muted small">Developed by <a href="https://github.com/dox">Andrew Breakspear</a>
+								<?php
+								if (debug) {
+									$output  = "<span class=\"badge text-bg-warning\">DEBUG MODE</span>";
+									echo $output;
+								}
+								?>
+								</p>
+							</div>
+							
 						</form>
 					</div>
 				</div>
