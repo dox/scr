21ddc26073292a2ce638110b4555b8e27383ea9c
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2023-10-10 19:34:16 +0100

    Proper navbar dropdown for dark mode.  Fixes #50

--- views/header.php
+++ views/header.php
@@ -48,40 +48,37 @@
 			echo "</div>";
 			?>
 		<div class="d-flex">
-			<div class="dropdown bottom-0 end-0 me-3 bd-mode-toggle">
-			  <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-					  id="bd-theme"
-					  type="button"
-					  aria-expanded="false"
-					  data-bs-toggle="dropdown"
-					  aria-label="Toggle theme (auto)">
-				<svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="img/icons.svg#dark-mode"></use></svg>
-				<span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-			  </button>
-			  <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-				<li>
-				  <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-					<svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="img/icons.svg#light-mode"></use></svg>
-					Light
-					<svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-				  </button>
-				</li>
-				<li>
-				  <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-					<svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="img/icons.svg#dark-mode"></use></svg>
-					Dark
-					<svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-				  </button>
-				</li>
-				<li>
-				  <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-					<svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="img/icons.svg#auto-mode"></use></svg>
-					Auto
-					<svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-				  </button>
-				</li>
-			  </ul>
-			</div>
+			<ul class="navbar-nav me-auto mb-2 mb-md-0">
+			  <li class="nav-item dropdown me-2">
+				<a class="nav-link dropdown-toggle d-print-none theme-icon-active" id="bd-theme" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+					<svg width="1em" height="1em" class="text-muted">
+						<use xlink:href="img/icons.svg#dark-mode"/>
+					</svg><span class="visually-hidden" id="bd-theme-text">Toggle theme</span></a>
+				<ul class="dropdown-menu">
+				  <li><button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
+						  <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="img/icons.svg#light-mode"></use></svg>
+						  Light
+						  <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
+						</button>
+					</li>
+					<li>
+						<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
+							<svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="img/icons.svg#dark-mode"></use></svg>
+							Dark
+							<svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
+						  </button>
+					</li>
+					<li>
+						<button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
+							<svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="img/icons.svg#auto-mode"></use></svg>
+							Auto
+							<svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
+						  </button>
+					</li>
+				</ul>
+			  </li>
+			</ul>
+			  
 		</div>
 	    <div class="d-flex">
 	      <ul class="navbar-nav mr-auto">
