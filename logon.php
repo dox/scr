6b44b812a26d622b4d5d798af793617463f2703a
Andrew Breakspear <andrew@longhanborough.com>
2024-07-03 13:56:33 +0100

    Missed logon for jQuery removal

--- logon.php
+++ logon.php
@@ -72,15 +72,14 @@ if (isset($_GET['logout'])) {
 </body>
 </html>
 
-<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="https://help.seh.ox.ac.uk/assets/chat/chat.min.js"></script>
+<script src="https://help.seh.ox.ac.uk/assets/chat/chat-no-jquery.min.js"></script>
 <script>
-$(function() {
+(function() {
   new ZammadChat({
 	title: 'Need IT Support?',
 	background: '#6b7889',
 	fontSize: '12px',
 	chatId: 1
   });
-});
+})();
 </script>
