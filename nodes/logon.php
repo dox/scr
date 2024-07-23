b2978f6bdc50145b8bf799e04bee3ffe453eead3
Andrew Breakspear <andrew@longhanborough.com>
2024-07-23 15:29:13 +0100

    Updating logon page

--- nodes/logon.php
+++ nodes/logon.php
@@ -1,61 +1,63 @@
-<section class="vh-100">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-6">
-				<div class="px-5 ms-xl-4">
-					<img src="/img/logo.png" alt="Site Logo" width="100px" class="me-3 pt-5 mt-xl-4" />
-					<h1 class="pt-4"><?php echo site_name; ?></h1>
-					
-				</div>
-				
-				<div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-					
-					<form style="width: 23rem;" method="post" id="loginSubmit" action="index.php">
-						<div class="pt-1 mb-4 d-grid gap-2">
-							<?php
-							if (isset($_SESSION['logon_error']) && $_GET['logout'] != "true") {
-								echo "<div class=\"alert alert-warning\" role=\"alert\">";
-								echo $_SESSION['logon_error'];
-								echo "</div>";
-							}
-							?>
-						</div>
-						
-						<div class="pt-1 mb-4 d-grid gap-2">
-							<?php
-							if (debug) {
-								$output  = "<div class=\"alert alert-warning\" role=\"alert\"><strong>DEBUG MODE</strong> IS ENABLED</div>";
-								echo $output;
-							}
-							?>
-						</div>
-						
-						<div class=" mb-4">
-							<label class="form-label" for="username">Username</label>
-							<input type="text" id="username" name="username" class="form-control" required />
-						</div>
-						
-						<div class="mb-4">
-							<label class="form-label" for="password">Password</label>
-							<input type="password" id="password" name="password" class="form-control" required />
-						</div>
-						
-						<div class="mb-4 form-check">
-						  <input class="form-check-input" type="checkbox" id="remember_me" name="remember_me" value="true" id="flexCheckDefault">
-						  <label class="form-check-label" for="flexCheckDefault">Remember Me</label>
-						</div>
-						
-						<div class="pt-1 mb-4 d-grid gap-2">
-							<button class="btn btn-primary" type="submit">Login</button>
-							<p class="mb-4 pb-lg-2"><a class="text-muted" href="<?php echo reset_url; ?>">Forgot password?</a></p>
-						</div>
-					</form>
-				</div>
+<style>
+.divider:after,
+.divider:before {
+  content: "";
+  flex: 1;
+  height: 1px;
+  background: #eee;
+}
+</style>
+
+<div class="row d-flex justify-content-center align-items-center h-100">
+	<div class="col-md-9 col-lg-6 col-xl-6 my-lg-5 py-lg-5">
+		<img src="/img/logo.png" alt="Site Logo" class="img-fluid" width="30%" />
+		<h1 class="pt-4"><?php echo site_name; ?></h1>
+		<?php
+		if (debug) {
+			$output  = "<div class=\"alert alert-warning\" role=\"alert\"><strong>DEBUG MODE</strong> IS ENABLED</div>";
+			echo $output;
+		}
+		?>
+	</div>
+	
+	<div class="col-md-8 col-lg-6 col-xl-5 offset-xl-1 my-lg-5 py-lg-5">
+		<form method="post" id="loginSubmit" action="index.php">
+			<div class="divider d-flex align-items-center my-4">
+				<p class="text-center fw-bold mx-3 mb-0">Sign In</p>
 			</div>
 			
-			<div class="col-sm-6 px-0 d-none d-sm-block">
-				<img src="/img/cover.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: center;">
+			<div class="pt-1 mb-4 d-grid gap-2">
+				<?php
+				if (isset($_SESSION['logon_error']) && $_GET['logout'] != "true") {
+					echo "<div class=\"alert alert-warning\" role=\"alert\">";
+					echo $_SESSION['logon_error'];
+					echo "</div>";
+				}
+				?>
 			</div>
-		</div>
+			
+			<div class=" mb-4">
+				<label class="form-label" for="username">Username</label>
+				<input type="text" id="username" name="username" class="form-control form-control-lg" required />
+			</div>
+			
+			<div class="mb-4">
+				<label class="form-label" for="password">Password</label>
+				<input type="password" id="password" name="password" class="form-control form-control-lg" required />
+			</div>
+			
+			<div class="d-flex justify-content-between align-items-center">
+				<div class="mb-0 form-check">
+					<input class="form-check-input" type="checkbox" id="remember_me" name="remember_me" value="true" id="flexCheckDefault">
+					<label class="form-check-label" for="flexCheckDefault">Remember Me</label>
+				</div>
+				
+				<a href="<?php echo reset_url; ?>" class="text-body">Forgot password?</a>
+			</div>
+			
+			<div class="text-center d-grid text-lg-start mt-4 pt-2">
+				<button class="btn btn-primary btn-lg" type="submit">Login</button>
+			</div>
+		</form>
 	</div>
-</section>
+</div>
