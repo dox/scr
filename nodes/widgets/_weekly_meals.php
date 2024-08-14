96e033c188b3f0949d104aa46602e036b0c25f7c
Andrew Breakspear <andrew@longhanborough.com>
2024-08-14 13:20:31 +0100

    Notice when a week has no meals

--- nodes/widgets/_weekly_meals.php
+++ nodes/widgets/_weekly_meals.php
@@ -8,23 +8,36 @@ $currentWeek = $termsClass->currentWeek();
 
 $mealsClass = new meals();
 
+$output = "";
+$noMealsCount = 0;
+
 // iterate through the whole week (based on weekStartDate)
 for($i = 0; $i < 7; $i++){
   $date = strtotime("+$i day", strtotime($suppliedWeek));
-
-  echo "<h2 class=\"text-center mt-3\">" . date('l', $date) . " <span class=\"text-muted\">" . date('F jS', $date) . "</span></h2>";
-
-  echo "<div class=\"row row-cols-1 row-cols-md-3 justify-content-center\">";
-
+  
   $meals = $mealsClass->allByDate(date('Y-m-d', $date));
   $meals = array_reverse($meals);
 
-  foreach ($meals AS $meal) {
-    $mealObject = new meal($meal['uid']);
+  $output .= "<h2 class=\"text-center mt-3\">" . date('l', $date) . " <span class=\"text-muted\">" . date('F jS', $date) . "</span></h2>";
 
-    echo $mealObject->mealCard();
+  $output .= "<div class=\"row row-cols-1 row-cols-md-3 justify-content-center\">";
+  
+  if (count($meals) > 0) {
+    foreach ($meals AS $meal) {
+      $mealObject = new meal($meal['uid']);
+    
+      $output .= $mealObject->mealCard();
+    }
+  } else {
+    $noMealsCount++;
   }
-  echo "</div>";
+  
+  $output .= "</div>";
 }
 
+if ($noMealsCount >= 7) {
+  $output = "<h2 class=\"text-center p-3\">Meals for this week are not yet available</h2>";
+}
+
+echo $output;
 ?>
