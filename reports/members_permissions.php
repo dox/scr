96477a822eb3cd109d64e859714e6099285dd710
Andrew Breakspear <andrew@longhanborough.com>
2024-12-20 08:18:40 +0000

    Permissions report

---
+++ reports/members_permissions.php
@@ -0,0 +1,23 @@
+<?php
+
+//$grantedPermissions = explode(",", $memberObject->permissions);
+foreach (available_permissions() AS $permission => $description) {
+  echo "<h2>" . $permission . " <small class=\"text-body-secondary\">" . $description . "</small></h2>";
+  $sql  = "SELECT * FROM members";
+  $sql .= " WHERE permissions LIKE \"%" . $permission . "%\"";
+  $sql .= "";
+  
+  $members = $db->query($sql)->fetchAll();
+  foreach ($members AS $member) {
+    $member = new member($member['uid']);
+    echo "<p>" . $member->displayName() . " (" . $member->ldap . ")</p>";
+  }
+  
+  echo "<hr />";
+}
+
+$logArray['category'] = "report";
+$logArray['result'] = "success";
+$logArray['description'] = "[reportUID:" . $report['uid'] . "] run";
+$logsClass->create($logArray);
+?>
