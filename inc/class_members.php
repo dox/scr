c92c64545be3c7bc6a7843c30e8a4484038a6e3c
Andrew Breakspear <andrew@longhanborough.com>
2025-07-01 09:04:31 +0100

    Minor code improvements

--- inc/class_members.php
+++ inc/class_members.php
@@ -1,86 +1,95 @@
 <?php
 class members extends member {
   public function all() {
-    global $db;
-
-    $sql  = "SELECT *  FROM " . self::$table_name;
-    $sql .= " ORDER BY precedence ASC, lastname ASC, firstname ASC";
-
-    $terms = $db->query($sql)->fetchAll();
-
-    return $terms;
+      global $db;
+  
+      $sql = "SELECT * FROM " . self::$table_name;
+      $sql .= " ORDER BY precedence ASC, lastname ASC, firstname ASC";
+  
+      $members = $db->query($sql)->fetchAll();
+  
+      return $members;
   }
-
+  
   public function allEnabled($type = 'all') {
-    global $db;
-
-    $sql  = "SELECT *  FROM " . self::$table_name;
-    if ($type == "scr") {
-      $sql .= " WHERE type = 'SCR'";
-    } elseif ($type == "mcr") {
-      $sql .= " WHERE type = 'MCR'";
-    } else {
-      $sql .= " WHERE type = type";
-    }
-    $sql .= " AND enabled = 1";
-    $sql .= " ORDER BY -precedence DESC, lastname ASC, firstname ASC";
-
-    $terms = $db->query($sql)->fetchAll();
-
-    return $terms;
+      global $db;
+  
+      $sql = "SELECT * FROM " . self::$table_name;
+      $conditions = ["enabled = 1"];
+  
+      if (in_array(strtoupper($type), ['SCR', 'MCR'])) {
+          $conditions[] = "type = '" . strtoupper($type) . "'";
+      }
+      
+      $sql .= " WHERE " . implode(" AND ", $conditions);
+      $sql .= " ORDER BY precedence ASC, lastname ASC, firstname ASC";
+  
+      $members = $db->query($sql)->fetchAll();
+  
+      return $members;
   }
 
   public function allDisabled($type = 'all') {
-    global $db;
-
-    $sql  = "SELECT *  FROM " . self::$table_name;
-    if ($type == "scr") {
-      $sql .= " WHERE type = 'SCR'";
-    } elseif ($type == "mcr") {
-      $sql .= " WHERE type = 'MCR'";
-    } else {
-      $sql .= " WHERE type = type";
-    }
-    $sql .= " AND enabled = 0";
-    $sql .= " ORDER BY -precedence DESC, lastname ASC, firstname ASC";
-
-    $terms = $db->query($sql)->fetchAll();
-
-    return $terms;
+      global $db;
+  
+      $sql = "SELECT * FROM " . self::$table_name;
+      $conditions = ["enabled = 0"];
+  
+      if (in_array(strtoupper($type), ['SCR', 'MCR'])) {
+          $conditions[] = "type = '" . strtoupper($type) . "'";
+      }
+      
+      $sql .= " WHERE " . implode(" AND ", $conditions);
+      $sql .= " ORDER BY precedence DESC, lastname ASC, firstname ASC";
+  
+      $members = $db->query($sql)->fetchAll();
+  
+      return $members;
   }
 
   public function memberCategories() {
-    global $settingsClass;
-
-    $memberCategoriesMandatory = array("");
-    $memberCategoriesSettings = explode(",", $settingsClass->value('member_categories'));
-    $memberCategories = array_merge($memberCategoriesMandatory, $memberCategoriesSettings);
-
-    return $memberCategories;
+      global $settingsClass;
+  
+      $raw = $settingsClass->value('member_categories');
+      $memberCategoriesSettings = array_filter(
+          array_map('trim', explode(',', $raw)),
+          fn($v) => $v !== ''
+      );
+  
+      // Always start with mandatory empty string
+      $memberCategories = array_merge([''], $memberCategoriesSettings);
+  
+      return $memberCategories;
   }
-
+  
   public function memberTitles() {
-    global $settingsClass;
-
-    $memberTitlesMandatory = array("");
-    $memberTitlesSettings = explode(",", $settingsClass->value('member_titles'));
-    $memberTitles = array_merge($memberTitlesMandatory, $memberTitlesSettings);
-
-    return $memberTitles;
+      global $settingsClass;
+  
+      $raw = $settingsClass->value('member_titles');
+      $memberTitlesSettings = array_filter(
+          array_map('trim', explode(',', $raw)),
+          fn($v) => $v !== ''
+      );
+  
+      // Always start with mandatory empty string
+      $memberTitles = array_merge([''], $memberTitlesSettings);
+  
+      return $memberTitles;
   }
-
+  
   public function dietaryOptions() {
-    global $settingsClass;
-    
-    $dietaryOptions = explode(",", $settingsClass->value('meal_dietary'));
-    
-    if (!empty($dietaryOptions)) {
-      sort($dietaryOptions);
-    } else {
-      $dietaryOptions = array();
-    }
-    
-    return $dietaryOptions;
+      global $settingsClass;
+  
+      $raw = $settingsClass->value('meal_dietary');
+      $dietaryOptionsSettings = array_filter(
+          array_map('trim', explode(',', $raw)),
+          fn($v) => $v !== ''
+      );
+  
+      // Sort dietary options alphabetically
+      sort($dietaryOptionsSettings);
+  
+      return $dietaryOptionsSettings;
   }
 }
 ?>
--- inc/class_terms.php
+++ inc/class_terms.php
@@ -1,27 +1,25 @@
 <?php
 class terms extends term {
   public function all() {
-    global $db;
-
-    $sql  = "SELECT *  FROM " . self::$table_name;
-    $sql .= " ORDER BY date_start DESC";
-
-    $terms = $db->query($sql)->fetchAll();
-
-    return $terms;
+      global $db;
+  
+      $sql = "SELECT * FROM " . self::$table_name;
+      $sql .= " ORDER BY date_start DESC";
+  
+      $terms = $db->query($sql)->fetchAll();
+  
+      return $terms;
   }
-
+  
   public function checkIsInTerm($date = null) {
     // accounts for 0th week automatically
     // so the term dates on the site should start on 1st week (as given by Oxford)
 
     global $db;
 
-    $sql  = "SELECT *  FROM " . self::$table_name;
+    $sql  = "SELECT * FROM " . self::$table_name;
     $sql .= " WHERE DATE_SUB(date_start, INTERVAL 7 DAY) <= '" . $date . "' AND date_end >= '" . $date . "'";
     $sql .= " LIMIT 1";
-
-    //echo $sql;
 
     $term = $db->query($sql)->fetchAll();
 
@@ -56,30 +54,4 @@ class terms extends term {
     return $weeksArray;
   }
 }
-
-
-/*
- * Oxford Dates of Term
- * Version 2014-01
- *
- * A small function that will pass a date ('YYYY-mm-dd') and
- * providing that date falls within an Oxford term, it will
- * return the start date, end date and term name.
- *
- * Dates taken from http://www.ox.ac.uk/about_the_university/university_year/dates_of_term.html
- *
- * Made by Andrew Breakspear @ St Edmund Hall
- * Feel free to use this in anyway you want, with or without credit
-*/
-
-function check_in_range($start_date, $end_date, $date_from_user) {
-	// Convert to timestamp
-	$start_ts = strtotime($start_date);
-	$end_ts = strtotime($end_date);
-	$user_ts = strtotime($date_from_user);
-
-	// Check that user date is between start & end
-	return (($user_ts >= $start_ts) && ($user_ts <= $end_ts));
-}
-
 ?>
