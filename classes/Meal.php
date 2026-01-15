6f90fcdcb9d6af1e501ab17ab78c082e8641db24
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2026-01-15 15:15:29 +0000

    Folder location fix

--- classes/Meal.php
+++ classes/Meal.php
@@ -234,7 +234,7 @@ class Meal extends Model {
 	}
 	
 	public function photographURL(): string {
-		$urlPath  = '/new/uploads/meal_cards/';
+		$urlPath  = '/uploads/meal_cards/';
 		$filePath = $_SERVER['DOCUMENT_ROOT'] . $urlPath;
 	
 		// Choose the candidate filename (null or empty means default)
